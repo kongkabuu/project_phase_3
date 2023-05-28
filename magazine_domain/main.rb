bc08510e89ae18e0932b4dd8a6552a7c6e99ed26
paul kabuu <kongkabuu9@gmail.com>
2023-05-28 23:13:01 +0300

    created all classes

---
+++ magazine_domain/main.rb
@@ -0,0 +1,74 @@
+class Author
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+      @articles = []
+    end
+  
+    def articles
+      @articles
+    end
+  
+    def magazines
+      @articles.map { |article| article.magazine }.uniq
+    end
+  
+    def add_article(magazine, title)
+      article = Article.new(self, magazine, title)
+      @articles << article
+      article
+    end
+  
+    def topic_areas
+      magazines.map { |magazine| magazine.category }.uniq
+    end
+  end
+  
+  class Magazine
+    attr_accessor  :category, :name
+  
+    @@all = []
+  
+    def initialize(name, category)
+      @name = name
+      @category = category
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def self.find_by_name(name)
+      @@all.find { |magazine| magazine.name == name }
+    end
+  
+    def article_titles
+      Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+    end
+  
+    def contributing_authors
+      Article.all.select { |article| article.magazine == self }
+                  .group_by { |article| article.author }
+                  .select { |author, articles| articles.length > 2 }
+                  .keys
+    end
+  end
+  
+  class Article
+    attr_reader :author, :magazine, :title
+  
+    @@all = []
+  
+    def initialize(author, magazine, title)
+      @author = author
+      @magazine = magazine
+      @title = title
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  end
