2f032d34c1ad4de55ff4af1ba23d31d8901f9521
paul kabuu <kongkabuu9@gmail.com>
2023-06-05 16:34:13 +0300

    wote the methods down

--- product_review/app/models/product.rb
+++ product_review/app/models/product.rb
@@ -5,5 +5,21 @@ class Product < ActiveRecord::Base
   attr_accessor :title
   self.inheritance_column = :_type_disabled
 
+
+  def leave_review(user, star_rating, comment)
+    reviews.create(user: user, star_rating: star_rating, comment: comment)
+  end
+
+  def print_all_reviews
+    reviews.each do |review|
+      puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+    end
+  end
+
+  def average_rating
+    total_ratings = reviews.pluck(:star_rating)
+    total_ratings.sum / total_ratings.length.to_f
+  end
+
 end
 
